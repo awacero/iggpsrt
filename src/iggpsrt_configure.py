c837e0b0d7b72e65defa3b8aa22833b72ccc3468
Wilson Acero <acerowilson@gmail.com>
2025-06-16 16:08:23 -0500

    Add PEP8-compliant docstrings (#1)
    added pep documentation

--- src/iggpsrt_configure.py
+++ src/iggpsrt_configure.py
@@ -5,9 +5,18 @@ from pathlib import Path
 app_config_dir = os.path.join(os.path.dirname(__file__),'..','config')
 
 def configure_logging():
+    """Configure logging using the configuration file found in ``config``.
+
+    Returns
+    -------
+    logging.Logger
+        Configured logger instance.
+    """
 
     print("Start of logging configuration")
-    logging.config.fileConfig(Path(app_config_dir,'logging.ini'), disable_existing_loggers=True)
+    logging.config.fileConfig(
+        Path(app_config_dir, 'logging.ini'), disable_existing_loggers=True
+    )
     logger = logging.getLogger(__name__)
 
     logger.info(f"Logger configured was: {logging.getLogger().handlers}")
